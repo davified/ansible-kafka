556edc03586661d3c5e59e93e50a6103d77cbf0d
Kevin Yeung <github@yeung.online>
2017-12-31 13:12:02 +0800

    Provision ubuntu boxes with openjdk8 and download kafka

---
+++ bin/set-known-hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh-keyscan -p 2100 localhost|grep ssh-rsa >> ~/.ssh/known_hosts
+ssh-keyscan -p 2101 localhost|grep ssh-rsa >> ~/.ssh/known_hosts
+ssh-keyscan -p 2102 localhost|grep ssh-rsa >> ~/.ssh/known_hosts
+
